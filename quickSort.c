a6817778c587ba533676a301e5592a9fa3daccc2
Aswani Kumar Thiruvanghadan <aswani_kumar_t@yahoo.in>
2016-03-04 15:41:14 +0530

    Added files via upload

--- quickSort.c
+++ quickSort.c
@@ -1,53 +1,112 @@
 #include <stdio.h>
 #include <string.h>
-#define N 6
+#define N 7
 
 
-// A utility function to swap two elements
-void swap(int* a, int* b)
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#define N 7
+
+void displayList(int ar[N], int left, int right)
 {
-    int t = *a;
-    *a = *b;
-    *b = t;
+    for(int i= left; i <= right; i++)
+        printf("%d ", ar[i]);
+    printf("\n");
+    
 }
-
 
 int partition(int ar[N], int left, int right)
-{
-    int pivot = right;
-
-    printf("%s: pivot element %d\n\n", __FUNCTION__, ar[pivot]);
-    displayList(ar, N);
-    //getchar();
+{   
+    //displayList(ar, 0, N-1);
+    int pivot = left;
+    // printf("pivot element is %d\n", ar[left]);
+    int temp = 0;
     while(left < right) {
-        while(ar[left] < ar[pivot])
+        while(ar[left] <= ar[pivot])
             left++;
-        while(ar[right] > ar[pivot])
+        while(ar[pivot] < ar[right])
             right--;
         if(left < right) {
-            swap(&ar[left],&ar[right]);
-            displayList(ar, N);
+            temp = ar[left];
+            ar[left] = ar[right];
+            ar[right] = temp;
         }
     }
-    printf("%s: left %d , right %d \n",  __FUNCTION__, left, right);
-    swap(&ar[pivot],&ar[left]);
-    displayList(ar, N);
-    printf("%s: Mid-point is %d\n", __FUNCTION__, left);
-    return left;
+    temp = ar[pivot];
+    ar[pivot] = ar[right];
+    ar[right] = temp;
+    return right;
 }
 
-
-void quickSort(int ar[N], int l, int h)
-{
-    printf("%s: left %d , right %d \n",  __FUNCTION__, l, h);
-    if(l < h) {
-        int p = partition(ar, l, h);
-        if(l < p-1)
-        quickSort(ar, l , p-1);
-        if(p+1 < h)
-        quickSort(ar, p+1, h);
+void quickSort(int ar[N], int left, int right) {
+    if(left < right) {
+        
+        int p = partition(ar, left, right);
+        
+        quickSort(ar, left, p-1);
+        quickSort(ar, p+1, right);
+        displayList(ar, left, right);
+       // displayList(ar, left, right);
     }
 }
+int main(void) {
+    int ar[N] = {5, 8, 1, 3, 7, 9, 2};
+
+   quickSort(ar, 0, N-1);
+   // displayList(ar, 0, N-1);
+   
+   return 0;
+}
+
+// A utility function to swap two elements
+// void swap(int* a, int* b)
+// {
+    // int t = *a;
+    // *a = *b;
+    // *b = t;
+// }
+
+
+// int partition(int ar[N], int left, int right)
+// {
+    // int pivot = left;
+
+    // printf("%s: pivot element %d\n\n", __FUNCTION__, ar[pivot]);
+    // displayList(ar, N);
+    // //getchar();
+    // while(left < right) {
+        // while(ar[left] < ar[pivot])
+            // left++;
+        // while(ar[right] > ar[pivot])
+            // right--;
+        // if(left < right) {
+            // swap(&ar[left],&ar[right]);
+            // displayList(ar, N);
+        // }
+    // }
+    // printf("%s: left %d , right %d \n",  __FUNCTION__, left, right);
+    // swap(&ar[pivot],&ar[right]);
+    // displayList(ar, N);
+    // printf("%s: Mid-point is %d\n", __FUNCTION__, right);
+    // return right;
+// }
+
+
+// void quickSort(int ar[N], int l, int h)
+// {
+    // printf("%s: left %d , right %d \n",  __FUNCTION__, l, h);
+    // if(l < h) {
+        // int p = partition(ar, l, h);
+        // if(l < p-1)
+        // quickSort(ar, l , p-1);
+        // if(p+1 < h)
+        // quickSort(ar, p+1, h);
+    // }
+// }
 
 
 
@@ -73,20 +132,20 @@ void quickSort(int ar[N], int l, int h)
     return (i + 1);
 }
  */
-void displayList(int arr[N], int n)
-{
-    printf("list is : ");
-    for(int i = 0; i < n; i++) {
-        printf(" %d,", arr[i]);
-    }
-    printf("\n");
-}
+// void displayList(int arr[N], int n)
+// {
+    // printf("list is : ");
+    // for(int i = 0; i < n; i++) {
+        // printf(" %d,", arr[i]);
+    // }
+    // printf("\n");
+// }
 
-int main()
-{
-    int ar[N] = {4,5,9,2,3,1};
-    // int ar[N] = {1,5,4,2,3,6};
-    quickSort(ar, 0, N-1);
-    displayList(ar, N);
-    return 0;
-}
+// int main()
+// {
+    // int ar[N] = {5, 8, 1, 3, 7, 9, 2};
+    // // int ar[N] = {1,5,4,2,3,6};
+    // quickSort(ar, 0, N-1);
+    // displayList(ar, N);
+    // return 0;
+// }
